9fe8d1066ca0d02ae246a92bfbc3e2d3fb8a0b56
Marcelo Dias <marcelo.dias@cilia.com.br>
2023-10-02 01:28:35 -0300

    VES-4 Refactor

--- spec/models/vessel_spec.rb
+++ spec/models/vessel_spec.rb
@@ -8,8 +8,8 @@ describe Vessel do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name).case_insensitive }
-    it { should validate_length_of(:name).is_at_least(Vessel::MIN_NAME_LENGTH) }
-    it { should validate_length_of(:name).is_at_most(Vessel::MAX_NAME_LENGTH) }
+    it { should validate_length_of(:name).is_at_least(described_class::MIN_NAME_LENGTH) }
+    it { should validate_length_of(:name).is_at_most(described_class::MAX_NAME_LENGTH) }
     it { should validate_presence_of(:imo) }
     it { should validate_uniqueness_of(:imo) }
     it { should validate_length_of(:imo).is_equal_to(described_class::IMO_LENGTH)
